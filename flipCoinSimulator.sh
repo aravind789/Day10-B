3bab518e084d21cfd6dd0b997608ece48cfbfdc9
aravind789 <aravindkris89@gmail.com>
2020-07-03 10:11:10 +0530

    Simulator on while loop

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
-random=$(($RANDOM%2))
-if [ $random -eq 1 ]
-then
-	echo "HEADS WINS"
-else
-	echo "TAILS WINS"
-fi
-
+count=0
+number_of_total_heads=0
+number_of_total_tails=0
+read -p "Enter how many times to flip the coin " num
+while [ $count -lt $num ]
+do
+	random=$(($RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		echo "HEADS WON"
+		((number_of_total_heads++))
+	else
+		echo "TAILS WON"
+		((number_of_total_tails++))
+	fi
+		((count++))
+done
+echo "Head won $number_of_total_heads times"
+echo "Tail won $number_of_total_tails times"
