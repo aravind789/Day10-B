98dff500604ef67765bcee8872ae6dc4a44a708f
aravind789 <aravindkris89@gmail.com>
2020-07-03 11:55:42 +0530

    Last addition to the program

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,7 +2,8 @@
 count=0
 number_of_total_heads=0
 number_of_total_tails=0
-while [ $count -lt 21 ]
+limit=21
+while [ $count -lt $limit ]
 do
 	random=$(($RANDOM%2))
 	if [ $random -eq 1 ]
@@ -17,15 +18,23 @@ done
 if [ $number_of_total_heads -eq $number_of_total_tails ]
 then
 	echo "It is a tie"
-else
-	if [ $number_of_total_heads -eq $count ]
+	diff1=$(($number_of_total_heads-$number_of_total_tails))
+	diff2=$(($number_of_total_tails-$number_of_total_heads))
+	if [ $diff1 -ge 2 -o $diff2 -ge 2 ]
 	then
-		diff=$(($number_of_total_heads-$number_of_total_tails))
-		echo "HEADS WON BY DIFFERENCE:" $diff
+		((limit++))
 	else
-		diff=$(($number_of_total_tails-$number_of_total_heads))
-		echo "TAIL WON BY DIFFERENCE" $diff
+		break
 	fi
+fi
+
+if [ $number_of_total_heads -eq $count ]
+then
+	diff=$(($number_of_total_heads-$number_of_total_tails))
+	echo "HEADS WON BY DIFFERENCE:" $diff
+else
+	diff=$(($number_of_total_tails-$number_of_total_heads))
+	echo "TAIL WON BY DIFFERENCE" $diff
 fi
 
 echo "Number of Heads" $number_of_total_heads
