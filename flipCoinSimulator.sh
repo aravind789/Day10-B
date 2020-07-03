60c04bcc13f6d9101ab1dffeb26cbd46caa1cee3
aravind789 <aravindkris89@gmail.com>
2020-07-03 11:17:33 +0530

    loop untill 21 times is reached

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,19 +2,32 @@
 count=0
 number_of_total_heads=0
 number_of_total_tails=0
-read -p "Enter how many times to flip the coin " num
-while [ $count -lt $num ]
+while [ $count -lt 21 ]
 do
 	random=$(($RANDOM%2))
 	if [ $random -eq 1 ]
 	then
-		echo "HEADS WON"
-		((number_of_total_heads++))
-	else
-		echo "TAILS WON"
-		((number_of_total_tails++))
-	fi
 		((count++))
+		number_of_total_heads=$count
+	else
+		((count++))
+		number_of_total_tails=$count
+	fi
 done
-echo "Head won $number_of_total_heads times"
-echo "Tail won $number_of_total_tails times"
+if [ $number_of_total_heads -eq $number_of_total_tails ]
+then
+	echo "It is a tie"
+else
+	if [ $number_of_total_heads -eq $count ]
+	then
+		diff=$(($number_of_total_heads-$number_of_total_tails))
+		echo "HEADS WON BY DIFFERENCE:" $diff
+	else
+		diff=$(($number_of_total_tails-$number_of_total_heads))
+		echo "TAIL WON BY DIFFERENCE" $diff
+	fi
+fi
+
+echo "Number of Heads" $number_of_total_heads
+echo "Number of Tails" $number_of_total_tails
+
